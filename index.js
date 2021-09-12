c7a288cf2602e829d6fcd1ffb30c3e271bb2cb3c
Phumrapee Soenvanichakul <77825705+Jannnn1235@users.noreply.github.com>
2021-09-12 22:33:35 +0700

    Update and rename js to index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+function facebook() {
+    var copyText = document.getElementById("facebook");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+}
+function instagram() {
+    var copyText = document.getElementById("instagram");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+}
+function discord() {
+    var copyText = document.getElementById("discord");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+}
+function line() {
+    var copyText = document.getElementById("line");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+}
+function gmail() {
+    var copyText = document.getElementById("gmail");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+}
