2899f519511519a6b621f4b2bd4b13cbdc323b83
Phumrapee Soenvanichakul <77825705+Jannnn1235@users.noreply.github.com>
2021-09-12 22:28:54 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Phumrapee Soenvanichakul</title>
+    <link rel="icon" type="image/x-icon" href="#" /> <!-- ICON -->
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <script type='text/javascript'>
+        document.ondragstart = function() {
+            return false;
+        };
+    </script>
+</head>
+
+<body id="page-top" class="mainfont bgme">
+    <nav class="navbar navbar-expand-lg navbar-custom fixed-top bg-white shadow" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="javascript:history.go(-1)"><i class="fas fa-arrow-left"></i></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ms-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                    <?php include('nav.html');?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section class="page-section" id="index2">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="row" style="margin-top: 2%;">
+                        <div class="col-md-6">
+                            <div class="card text-white bg-blueme mb-3" style="max-width: 100%;">
+                                <div class="card-header"><i class="fab fa-facebook-square"></i> Facebook</div>
+                                <div class="card-body">
+                                    <input type="text" value="Phumrapee Soenvanichakul" id="facebook" hidden="true">
+                                    <h5 class="card-title">Phumrapee Soenvanichakul <a><i class="far fa-copy" onclick="facebook()"></i></h5>
+                                    <p class="card-text"><a target="_blank" href="https://github.com/Jannnn1235" style="text-decoration: none; color: white;">Click
+                                            Here</a></p>
+                                </div>
+                            </div>
+                            <div class="card text-white bg-secondary mb-3" style="max-width: 100%;">
+                                <input type="text" value="H O M I E#4972" id="discord" hidden="true">
+                                <div class="card-header"><i class="fab fa-discord"></i> Discord</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">H O M I E#4972 <i class="far fa-copy" onclick="discord()"></i></h5>
+                                    <p class="card-text"><a target="_blank" href="https://discord.gg/cF4W869vgJ" style="text-decoration: none; color: white;">Click
+                                            Here</a></p>
+                                </div>
+                            </div>
+                            <div class="card text-white bg-success mb-3" style="max-width: 100%;">
+                                <input type="text" value="phumhack" id="line" hidden="true">
+                                <div class="card-header"><i class="fab fa-line"></i> Line</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">ไอดีไลน์ : phumhack <i class="far fa-copy" onclick="line()"></i></h5>
+                                    <p class="card-text"><a target="_blank" href="https://line.me/ti/p/OdCknBnUI9" style="text-decoration: none; color: white;">Click
+                                            Here</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card text-white bg-danger mb-3" style="max-width: 100%;">
+                                <input type="text" value="phumrapeephum" id="instagram" hidden="true">
+                                <div class="card-header"><i class="fab fa-instagram"></i> Instagram</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">phumrapeephum <i class="far fa-copy" onclick="instagram()"></i></h5>
+                                    <p class="card-text"><a target="_blank" href="https://www.instagram.com/phumrapeephum/" style="text-decoration: none; color: white;">Click
+                                            Here</a></p>
+                                </div>
+                            </div>
+                            <div class="card text-white bg-warning mb-3" style="max-width: 100%;">
+                                <input type="text" value="phumrapeesoen1@gmail.com" id="gmail" hidden="true">
+                                <div class="card-header"><i class="fas fa-envelope"></i> Gmail</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">phumrapeesoen1@gmail.com <i class="far fa-copy" onclick="gmail()"></i></h5>
+                                    <p class="card-text"><a target="_blank" href="mailto:phumrapeesoen1@gmail.com" style="text-decoration: none; color: white;">Click
+                                            Here</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="js/dontclick.js"></script>
+    <script src="js/index.js"></script>
+    <script src="main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    <script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/scrolltopcontrol.js"></script>
+</body>
+
+<footer class="footer">
+    <?php include('footer.html') ?>
+</footer>
+
+</html>
